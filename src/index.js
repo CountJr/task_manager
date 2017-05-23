4e7f22a0201f5e7fb50e0ab5747eb605f2739ed2
Kirill Grafov <countjr@gmail.com>
2017-05-23 19:23:39 +0200

    add sessions and responsive navbar with session info

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 // @flow
-
+import 'babel-polyfill';
 import Koa from 'koa';
 import Router from 'koa-router';
 import Pug from 'koa-pug';
 import middleware from 'koa-webpack';
+import session from 'koa-session';
 import _ from 'lodash';
 import path from 'path';
 // import rollbar from 'rollbar';
@@ -18,13 +19,12 @@ export default () => {
 
   const app = new Koa();
 
-  app.keys = ['some1', 'some2'];
-  const config = { signed: false };
+  app.keys = ['jopa'];
+  const CONFIG = { key: 'koases', signed: true };
 
   const container = {};
   const router = new Router();
   addRoutes(router, container);
-
 
   const pug = new Pug({
     viewPath: path.join(__dirname, 'views'),
@@ -40,11 +40,26 @@ export default () => {
   });
 
   app
+    // TODO: keep session in sequelize: koa-generic-session-sequelize + koa-generic-session
+    .use(session(CONFIG, app))
+    .use(async (ctx, next) => {
+      // ignore favicon
+      if (ctx.path === '/favicon.ico') return;
+
+      ctx.state = {
+        isSignedIn: () => Boolean(ctx.session.userID),
+        userID: ctx.session.userID,
+      }
+
+      // delete session: ctx.session = null;
+      await next();
+    })
     .use(middleware({
       config: getWebpackConfig(),
     }))
     .use(router.routes())
     .use(router.allowedMethods());
+
   pug.use(app);
 
   return app;
